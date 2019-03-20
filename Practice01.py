2a93855e4e0ec82598216087b4f7c133acc876d8
ninja250 <naitomea.mky@gmail.com>
2019-03-21 03:41:08 +0900

    Update Practice01.py

---
+++ Practice01.py
@@ -0,0 +1,12 @@
+# for
+    for countdown in 5, 4, 3, 2, 1, "hey!":
+        print(countdown)
+
+# 辞書
+    kimoti = {
+        "oko"   :   "kusoyarou",
+        "yoki"  :   "naa!!",
+        "iya"   :   "iyaya",
+    }
+        sizuno = "iya"
+        print(sizuno,  "says:",  kimoti[sizuno]   )
